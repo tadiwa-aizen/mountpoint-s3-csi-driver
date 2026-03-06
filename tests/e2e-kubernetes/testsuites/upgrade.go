b7c09a9a85c956dd25da721487418d66d6bba67d
Tadiwa Magwenzi <tadiwaom@amazon.com>
2026-03-06 01:46:16 +0000

    v1-v2-test
    Signed-off-by: Tadiwa Magwenzi <tadiwaom@amazon.com>

--- tests/e2e-kubernetes/testsuites/upgrade.go
+++ tests/e2e-kubernetes/testsuites/upgrade.go
@@ -355,6 +355,7 @@ func buildHelmValues() map[string]any {
 				"enable": "true",
 			},
 		},
+		"supportLegacySystemDMounts": false,	
 	}
 	if helmChartContainerRepository != "" && helmChartContainerTag != "" {
 		values["image"] = map[string]any{
